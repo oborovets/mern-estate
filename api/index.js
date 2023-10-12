b2175f572e30e882159b406e0c22547b7da30e39
Orest Borovets <borovets.orest@gmail.com>
2023-10-12 19:50:24 +0300

    Connect to DB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,12 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => console.log("Connected to DB"))
+  .catch((e) => console.log(e));
 
 const app = express();
 
