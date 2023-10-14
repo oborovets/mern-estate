6da61b58d5da36c411cbbf982ed2161806b2140b
Orest Borovets <borovets.orest@gmail.com>
2023-10-14 20:00:28 +0300

    Complete upload listing images functionality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,4 +1,61 @@
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { useState } from "react";
+import { app } from "../firebase";
+
 export default function CreateListing() {
+  const [files, setFiles] = useState([]);
+  const [formData, setFormData] = useState({
+    imageUrls: [],
+  });
+  const [imageUploadError, setImageUploadError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const storeImage = (file) => {
+    return new Promise((res, rej) => {
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + file.name;
+      const storageRef = ref(storage, fileName);
+      const uplaodTask = uploadBytesResumable(storageRef, file);
+
+      uplaodTask.on("state_changed", null, rej, () => {
+        getDownloadURL(uplaodTask.snapshot.ref).then((downloadURL) => {
+          res(downloadURL);
+        });
+      });
+    });
+  };
+
+  const handleImageSubmit = () => {
+    if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
+      setLoading(true);
+      setImageUploadError(false);
+      const promises = [...files].map((file) => storeImage(file));
+
+      Promise.all(promises)
+        .then((imageUrls) => {
+          setFormData({
+            ...formData,
+            imageUrls: formData.imageUrls.concat(imageUrls),
+          });
+        })
+        .catch(() => setImageUploadError("Image upload failed."))
+        .finally(() => setLoading(false));
+    } else {
+      setLoading(false);
+      setImageUploadError("You can upload 6 images per listing");
+    }
+  };
+  const handleRemoveImage = (idx) => {
+    setFormData({
+      ...formData,
+      imageUrls: formData.imageUrls.filter((_, i) => i !== idx),
+    });
+  };
   return (
     <main className="p-3 max-w-4xl mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">
@@ -109,16 +166,45 @@ export default function CreateListing() {
           </p>
           <div className="flex gap-4">
             <input
+              onChange={(e) => setFiles(e.target.files)}
               className="p-3 border border-gray-300 rounded w-full"
               type="file"
               id="images"
               accept="image/*"
               multiple
             />
-            <button className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80">
-              Upload
+            <button
+              disabled={loading}
+              type="button"
+              onClick={handleImageSubmit}
+              className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80"
+            >
+              {loading ? "Uploading..." : "Upload"}
             </button>
           </div>
+          <p className="text-red-700 text-sm">
+            {imageUploadError ? imageUploadError : null}
+          </p>
+          {formData.imageUrls.length > 0 &&
+            formData.imageUrls.map((url, idx) => (
+              <div
+                key={url}
+                className="flex justify-between p-3 border item-center"
+              >
+                <img
+                  src={url}
+                  alt="listing image"
+                  className="w-20 h-20 object-contain rounded-lg"
+                />
+                <button
+                  onClick={() => handleRemoveImage(idx)}
+                  type="button"
+                  className="p-3 text-red-700 rounde-lg uppercase hover:opacity-75"
+                >
+                  Delete
+                </button>
+              </div>
+            ))}
           <button className="p-3 bg-slate-700 border text-white border-green-700 rounded-lg uppercase hover:shadow-lg disabled:opacity-80">
             Create Listing
           </button>
