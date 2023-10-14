3d9e9a297e6bd87ef30c6883a20eecbb904dd272
Orest Borovets <borovets.orest@gmail.com>
2023-10-14 13:00:11 +0300

    Complete image upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,16 +1,92 @@
+import { useRef, useState, useEffect } from "react";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
 import { useSelector } from "react-redux";
 
+import { app } from "../firebase";
+
+// eslint-disable-next-line react/prop-types
+const AvatarProgress = ({ fileUploadError, filePerc }) => {
+  if (fileUploadError)
+    return (
+      <p className="text-slate-700 test-sm self-center">
+        Error while uploading image
+      </p>
+    );
+
+  if (filePerc > 0 && filePerc < 100)
+    return (
+      <p className="text-slate-700 test-sm self-center">
+        Uploading {filePerc}%
+      </p>
+    );
+
+  if (filePerc === 100)
+    return (
+      <p className="text-green-700 test-sm self-center">
+        Image successfully uploaded
+      </p>
+    );
+};
+
 export default function Profile() {
+  const fileRef = useRef(null);
+  const [file, setFile] = useState(undefined);
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [formData, setFormData] = useState({});
   const { currentUser } = useSelector((state) => state.user);
+
+  const handlerFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePerc(Math.round(progress));
+      },
+      () => setFileUploadError(true),
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) =>
+          setFormData({ ...formData, avatar: downloadUrl })
+        );
+      }
+    );
+  };
+
+  useEffect(() => {
+    if (file) {
+      handlerFileUpload(file);
+    }
+  }, [file]);
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
       <form className="flex flex-col gap-4">
+        <input
+          onChange={(e) => setFile(e.target.files[0])}
+          type="file"
+          ref={fileRef}
+          hidden
+          accept="image/*"
+        />
         <img
+          onClick={() => fileRef.current.click()}
           className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
-          src={currentUser.avatar}
+          src={formData.avatar || currentUser.avatar}
           alt="avatar"
         />
+        <AvatarProgress filePerc={filePerc} fileUploadError={fileUploadError} />
         <input
           type="text"
           placeholder="Username"
