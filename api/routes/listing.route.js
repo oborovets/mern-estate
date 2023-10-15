cf271f6cb64bf93c92dee18ff7e6784908f1618a
Orest Borovets <borovets.orest@gmail.com>
2023-10-15 19:58:15 +0300

    Create update listing API route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -3,12 +3,14 @@ import express from "express";
 import {
   createListing,
   deleteListing,
+  updateListing,
 } from "../controller/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createListing);
+router.post("/update/:id", verifyToken, updateListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
 
 export default router;
