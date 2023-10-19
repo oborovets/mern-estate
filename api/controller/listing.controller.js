76748c663d803efad431e5bc1925af06265b8d2f
Orest Borovets <borovets.orest@gmail.com>
2023-10-19 20:11:33 +0300

    Fix broken search

--- api/controller/listing.controller.js
+++ api/controller/listing.controller.js
@@ -18,20 +18,25 @@ export const getListings = async (req, res, next) => {
   try {
     const limit = parseInt(req.query.limit) || 9;
     const startIndex = parseInt(req.query.startIndex) || 0;
+
     let offer = req.query.offer;
     let furnished = req.query.furnished;
     let parking = req.query.parking;
-    let type = req.query.type;
-    const searchTerm = req.query.searchTerm || "";
-    const sort = req.query.sort || "createdAt";
-    const order = req.query.order || "desc";
-    if (!offer) {
+
+    let {
+      type = "all",
+      searchTerm = "",
+      sort = "createdAt",
+      order = "desc",
+    } = req.query;
+
+    if (offer === undefined || offer === "false") {
       offer = { $in: [false, true] };
     }
-    if (!furnished) {
+    if (furnished === undefined || furnished === "false") {
       furnished = { $in: [false, true] };
     }
-    if (!parking) {
+    if (parking === undefined || parking === "false") {
       parking = { $in: [false, true] };
     }
     if (!type || type === "all") {
@@ -48,6 +53,7 @@ export const getListings = async (req, res, next) => {
       .sort({ [sort]: order })
       .limit(limit)
       .skip(startIndex);
+
     return res.status(200).json(listings);
   } catch (error) {
     next(error);
