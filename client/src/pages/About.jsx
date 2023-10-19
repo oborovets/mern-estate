6d01ebb491dabf97af53bc7e9f1f6f69a8d6f1e3
Orest Borovets <borovets.orest@gmail.com>
2023-10-19 16:01:19 +0300

    Complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,3 +1,57 @@
 export default function about() {
-  return <div>about</div>;
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto text-slate-700">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Lviv Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Welcome to Lviv Estate, where we aim to redefine the way you experience
+        real estate. Our mission is to provide a seamless, innovative, and
+        personalized approach to the world of property management, buying,
+        selling, and investing.
+      </p>
+      <p className="mb-4 text-slate-700">🏡 Our Vision</p>
+      <p className="mb-4 text-slate-700">
+        We envision a future where everyone can effortlessly navigate the
+        complex real estate landscape, making informed decisions that match
+        their unique needs and aspirations. Whether you&apos;re a first-time
+        homebuyer, an experienced investor, or a property owner, we&apos;re here
+        to simplify your journey and empower you with the knowledge and tools
+        you need.
+      </p>
+      <p className="mb-4">🌟 Why Choose Lviv Estate?</p>
+      <p className="mb-4">
+        At Lviv Estate, we pride ourselves on a few key principles that set us
+        apart:
+      </p>
+      <ul>
+        <li className="mb-2">
+          <strong>Innovation:</strong> We leverage cutting-edge technology to
+          streamline processes, making property transactions smoother and more
+          efficient.
+        </li>
+        <li className="mb-2">
+          <strong>Personalization:</strong> We understand that every real estate
+          journey is unique. Our platform tailors recommendations and solutions
+          to your specific requirements.
+        </li>
+        <li className="mb-2">
+          <strong>Transparency:</strong> We believe in open, honest, and clear
+          communication. You&apos;ll always know where you stand in your real
+          estate journey.
+        </li>
+        <li className="mb-2">
+          <strong>Expertise:</strong> Our team is composed of seasoned
+          professionals who bring a wealth of experience in the real estate
+          industry. We&apos;re here to guide and support you at every step.
+        </li>
+      </ul>
+      <p>
+        Thank you for choosing Lviv Estate as your partner in your real estate
+        journey. We look forward to being a part of your story, helping you find
+        your dream home, make smart investments, or manage your properties with
+        ease.
+      </p>
+    </div>
+  );
 }
